dfd606766a3083aa52aa4b3ca3f90a5b4301caf3
Yurim Jin <jayjinjay@gmail.com>
2020-07-19 18:07:25 +0900

    geolocation 삽입

--- src/pages/Home/components/PositionCard.js
+++ src/pages/Home/components/PositionCard.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MapIcon } from "reusables/Icons";
 import Divider from "reusables/Divider";
 import styled from "@emotion/styled";
 import { css } from "emotion";
+import useGeolocation from "react-hook-geolocation";
 
 export default () => {
+  const [regionValue, setRegionValue] = useState(null);
+  const geolocation = useGeolocation();
+
+  useEffect(() => {
+    const getRegion = async (lat, lng) => {
+      const region = await getGeo(lat, lng);
+      setRegionValue(region);
+    };
+
+    if (geolocation.latitude != null && regionValue === null) {
+      setRegionValue("");
+      getRegion(geolocation.latitude, geolocation.longitude);
+    }
+  }, [geolocation, regionValue]);
+
   return (
     <Container>
       <div
@@ -15,8 +31,9 @@ export default () => {
         `}
       >
         <h2>
-          You see stories in <br />
-          Seoul (+1km)
+          You see stories in
+          <br />
+          {regionValue} (+1km)
         </h2>
         <MapIcon />
       </div>
@@ -28,3 +45,23 @@ export default () => {
 const Container = styled.div`
   margin-top: 40px;
 `;
+
+const getGeo = async (lat, lng) => {
+  return await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyDrx5BO-OfUWYDuqmhqFlXCKgMpaRLpbYI`,
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      console.log({ myJson }, myJson.plus_code);
+      if (myJson.plus_code === undefined) {
+        return "";
+      }
+      const regionText = myJson.plus_code.compound_code;
+      return regionText
+        .split(" ")
+        .splice(2)
+        .join(" ");
+    });
+};
